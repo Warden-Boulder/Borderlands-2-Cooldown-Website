255d9fcbd650adc075f07506b31c572c0077f9e3
Boulder <157924017+Derek-Stafford@users.noreply.github.com>
2025-05-02 15:28:24 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function CalculateTotal() {
+  let baseCooldown = parseFloat(document.getElementById("Action").value) || 0;
+  let skills = parseFloat(document.getElementById("Skills").value) || 0;
+  let classMod = parseFloat(document.getElementById("class_mod").value) || 0;
+  let relic = parseFloat(document.getElementById("Relic").value) || 0;
+
+  let total = (skills + classMod + relic) / 100;
+  let effective = baseCooldown * (1 - (total / (1 + total)));
+  
+  let resultsHTML = "<h3>Result:</h3>";
+  resultsHTML += "<p>Effective Cooldown = " + effective.toFixed(2) + " seconds</p>";
+  document.getElementById("results").innerHTML = resultsHTML;
+}
