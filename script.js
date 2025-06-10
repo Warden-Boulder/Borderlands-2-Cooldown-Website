063b1046bcce72766bc84a288b3f89a0f6f8d62d
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-06-09 22:58:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function CalculateTotal() {
-  let baseCooldown = parseFloat(document.getElementById("Action").value) || 0;
-  let skills = parseFloat(document.getElementById("Skills").value) || 0;
+  let baseCooldown = parseFloat(document.getElementById("action").value) || 0;
+  let skills = parseFloat(document.getElementById("skills").value) || 0;
   let classMod = parseFloat(document.getElementById("class_mod").value) || 0;
-  let relic = parseFloat(document.getElementById("Relic").value) || 0;
+  let relic = parseFloat(document.getElementById("relic").value) || 0;
 
   let total = (skills + classMod + relic) / 100;
   let effective = baseCooldown * (1 - (total / (1 + total)));
   
   let resultsHTML = "<h3>Result:</h3>";
-  resultsHTML += "<p>Effective Cooldown is about " + effective.toFixed(2) + " seconds</p>";
+  resultsHTML += "<p>Effective Cooldown is " + effective.toFixed(2) + " seconds</p>";
   document.getElementById("results").innerHTML = resultsHTML;
 }
