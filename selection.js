0bf0713d9038716d7572aef4d897c1d01e67c557
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-07-11 12:32:33 -0700

    Update selection.js

--- selection.js
+++ selection.js
@@ -5,25 +5,32 @@ function toggleButtons() {
   const character = characterSelect.value;
   
   let cooldownValue = "";
+  let actionskill = "";
   
   if (character === "axton") {
+    actionskill = "Sabre Turret";
     cooldownValue = 36;
   } else if (character === "gaige") {
+    actionskill = "Deathtrap"
     cooldownValue = 60;
   } else if (character === "krieg") {
+    actionskill = "Buzz Axe Rampage";
     cooldownValue = 120;
   } else if (character === "maya") {
+    actionskill = "Phaselock";
     cooldownValue = 13;
   } else if (character === "salvador") {
+    actionskill = "Gunzerking";
     cooldownValue = 42;
   } else if (character === "zero") {
+    actionskill = "Decepti0n";
     cooldownValue = 15;
   }
   
   if (cooldownValue) {
     actionInput.value = cooldownValue;
     const characterName = character.charAt(0).toUpperCase() + character.slice(1);
-    cooldownText.textContent = `Vault Hunter: ${characterName}\nBase Cooldown: ${cooldownValue} seconds`;
+    cooldownText.textContent = `Vault Hunter: ${characterName}\nAction Skill: ${actionskill}\nBase Cooldown: ${cooldownValue} seconds`;
   } else {
     actionInput.value = "";
     cooldownText.textContent = "Base Cooldown is";
