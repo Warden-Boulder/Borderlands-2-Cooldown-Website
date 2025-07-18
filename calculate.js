4ac203c34cde514f49842d3fa728fdfab38ed98b
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-07-18 15:49:46 -0700

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -3,11 +3,12 @@ function CalculateTotal() {
   let skills = parseFloat(document.getElementById("skills").value) || 0;
   let classMod = parseFloat(document.getElementById("class_mod").value) || 0;
   let relic = parseFloat(document.getElementById("relic").value) || 0;
+  let flat = parseFloat(document.getElementById("additional").value) || 0;
 
   let total = (skills + classMod + relic) / 100;
   let effective = baseCooldown * (1 - (total / (1 + total)));
+  let additional = effective-flat;
   
-  let resultsHTML = "<h3>Result:</h3>";
-  resultsHTML += "<p>Effective Cooldown is " + effective.toFixed(2) + " seconds</p>";
+  let resultsHTML = "<p>Effective Cooldown:<br>" + effective.toFixed(2) + " seconds (staticly)<br> ~ 0.0 seconds to " + effective.toFixed(2) + " seconds (pending skill use)</p>";
   document.getElementById("results").innerHTML = resultsHTML;
 }
