3c52fc62da97bd88d43420d018d03e4544a1731f
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2026-06-15 02:05:09 -0700

    Add navigation function to handle page redirects

---
+++ navigation.js
@@ -0,0 +1,16 @@
+function nav() {
+   let navigationSelect = document.querySelector('nav');
+   let selectedValue = navigationSelect.value;
+
+   switch(selectedValue) {
+     case 'home':
+       window.location.href = 'index.html';
+       break;
+     case 'calculator':
+       window.location.href = 'calculators.html';
+       break;
+       case 'road':
+       window.location.href = 'roadmap.html';
+       break;
+   }
+}
